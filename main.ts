2d5d4d8216b1fe1dab39849cd3d1ae8b8ccd534c
Conor-Hinchee <conorphinchee@gmail.com>
2025-02-26 10:49:33 -0500

    fix/timestamp-replace2

--- main.ts
+++ main.ts
@@ -8,19 +8,15 @@ export default class QuickTimestamper extends Plugin {
 
             button.addEventListener("click", async () => {
                 const now = new Date();
-
-                const year = now.getFullYear();
-                const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
-                const day = String(now.getDate()).padStart(2, '0');
                 const hours = String(now.getHours()).padStart(2, '0');
                 const minutes = String(now.getMinutes()).padStart(2, '0');
                 const seconds = String(now.getSeconds()).padStart(2, '0');
 
-                const timestamp = `Logged at : ${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+                const timestamp = `🪵 Logged at: ${hours}:${minutes}:${seconds}`;
 
                 const view = this.app.workspace.getActiveViewOfType(MarkdownView);
                 if (view) {
-                    await view.editor.replaceRange(timestamp, view.editor.getCursor());
+                    view.editor.replaceRange(timestamp, view.editor.getCursor());
                     button.remove(); // Remove the button after logging the timestamp
                 }
             });
